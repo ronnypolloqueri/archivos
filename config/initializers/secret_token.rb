ba810bc4cc5879e45a0c21fcba9f1bb26b541938
Ronny R. Polloqueri <ronny.polloqueri@gmail.com>
2014-01-13 19:26:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbriendoArchivos::Application.config.secret_key_base = 'e5cdd409b09d78c9b383731fc1167283f4ae1f3807b289c1cf1b169e8b0c0a80e32f6309f611eac44dff11fa3839a7d2f6bc7efb94d6a5a49dcac0ee4ed68461'
